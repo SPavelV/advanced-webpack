6a8411a75ba928ced5b8150a4cdd983db150a419
Pavel Solovev <pashius2010@gmail.com>
2020-11-19 09:24:01 +0400

    add postCSS

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -1,5 +1,6 @@
 // Core
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const env = require("postcss-preset-env");
 
 const { SOURCE_DIRECTORY, BUILD_DIRECTORY } = require("../constants");
 
@@ -22,7 +23,52 @@ module.exports = () => {
         {
           // TODO: прокачать загрузку стилей
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    // цепочка плагинов postcss
+                    env({
+                      stage: 0, // default: stage 2
+                      features: {
+                        "custom-media-queries": {
+                          importFrom: [
+                            {
+                              customMedia: {
+                                "--phonePortrait": "(width <= 414px)",
+                                "--phoneLandscape":
+                                  "(width => 415px) and (width <= 667px)",
+                                "--tabletPortrait":
+                                  "(width => 668px) and (width <= 768px)",
+                                "--tabletLandscape":
+                                  "(width => 769px) and (width <= 1024px)",
+                                "--desktopS":
+                                  "(width => 1025px) and (width <= 1366px)",
+                                "--desktopM":
+                                  "(width => 1367px) and (width <= 1680px)",
+                                "--desktopL":
+                                  "(width => 1681px) and (width <= 1920px)",
+                                "--desktopXL": "(width >= 1921px)",
+                              },
+                            },
+                          ],
+                        },
+                      },
+                    }),
+                  ],
+                },
+              },
+            },
+          ],
         },
       ],
     },
--- source/react-components/index.js
+++ source/react-components/index.js
@@ -1,23 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Styles
+import Styles from "./postcss.css";
+
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
+    this.state = { isToggleOn: true, count: 34 };
   }
 
   handleClick = () => {
     this.setState((state) => ({
       isToggleOn: !state.isToggleOn,
+      count: ++state.count,
     }));
   };
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? "Включено!!" : "Выключено"}
-      </button>
+      <section
+        className={Styles.toggler}
+        style={{
+          "--mainColor": "rebeccapurple",
+          "--headingFontSize": `${this.state.count}px`,
+        }}
+      >
+        <h1>Test:</h1>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "Включено!!" : "Выключено"}
+        </button>
+      </section>
     );
   }
 }
