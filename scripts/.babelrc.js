021b3281ec6ebf88dad96997355d1a5f09d2b0ea
Pavel Solovev <pashius2010@gmail.com>
2020-11-24 08:27:37 +0400

    add ecma modules for webpack

---
+++ scripts/.babelrc.js
@@ -0,0 +1,27 @@
+module.exports = (api) => {
+  const env = api.env();
+
+  api.cache.using(() => env === "development");
+
+  const plugins = ["dynamic-import-node"];
+
+  return {
+    presets: [
+      [
+        "@babel/env",
+        {
+          useBuiltIns: "usage",
+          shippedProposals: true,
+          spec: true,
+          loose: false,
+          debug: false,
+          corejs: 3,
+          targets: {
+            node: "current",
+          },
+        },
+      ],
+    ],
+    plugins,
+  };
+};
